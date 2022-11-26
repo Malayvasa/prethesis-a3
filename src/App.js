ad5c7e1b9796a133d97dbd2ec5d4c14d46f6b1e8
malayvasa <malayvasa@gmail.com>
2022-11-26 17:05:52 +0530

    Commit Two

--- src/App.js
+++ src/App.js
@@ -28,23 +28,91 @@ function App() {
 
       <div className="text-3xl mx-12 md:text-5xl w-1/2">In this bunch, there was a monkey in the corner.</div>
       
-      <div className="flex flex-col text-3xl md:text-5xl">
+      <div className="flex flex-col text-3xl md:text-5xl items-center">
         <div className="flex-shrink-0">
-          Even though 
+          Even though he was 
         </div>
         
         <div className="">
-          he was <span className="text-8xl inline-block shake-little shake-constant"> shivering,</span>
+           <span className="text-8xl inline-block shake-little shake-constant"> shivering,</span>
         </div>
 
         <img className="md:h-[512px] md:w-[512px] aspect-auto mix-blend-multiply w-auto shake-little shake-constant" src="images/monkey.png"></img>
-      </div>
-
       
-      
-      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        <div className="text-3xl mx-12 md:text-5xl w-1/2">
           he did not sit with the others.
       </div>
+      </div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        The animals, all wanted to find an escape from the <span className="shake-little shake-constant">cold.</span> After a lot of discussion, they decided to build a bonfire.
+      </div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        The monkey remembered that he had seen a bunch of humans build a bonfire before.
+      </div>
+
+      <div className="flex flex-col items-center">
+        <div className="text-3xl mx-12 md:text-5xl w-1/2">
+          He needed three things to build one :
+        </div>
+      
+        <div className="text-3xl mx-12 md:text-5xl">
+          stones, sticks and dry hay.
+        </div>
+
+        <div className="flex flex-row pt-32">
+        <div>
+          <img className="md:h-[450px] md:w-[450px] aspect-auto mix-blend-multiply w-auto" src="images/stones.png"></img>
+        </div>
+
+        <div>
+          <img className="md:h-[450px] md:w-[450px] aspect-auto mix-blend-multiply w-auto" src="images/wood.png"></img>
+        </div>
+
+        <div>
+          <img className="md:h-[450px] md:w-[450px] aspect-auto mix-blend-multiply w-auto" src="images/hay.png"></img>
+        </div>
+      </div>
+      </div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        He decided to set out on building a perfect bonfire all by himself.
+      </div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        To get the first ingredient, he went to the lake.
+      </div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        Although he did not know how to swim he was determined to get the stones from the bottom of the lake himself.
+      </div>
+
+      <img className="md:h-[725px] md:w-[1450px] aspect-auto mix-blend-multiply w-auto" src="images/lake.png"></img>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        Seeing how well the crocodile was swimming, he tries to copy him. 
+      </div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        After several failed attempts he was approached by the otter.
+      </div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        The Otter offers to help the monkey by getting him the large stones from the bottom of the lake. 
+      </div>
+
+      <img className="md:h-[725px] md:w-[1450px] aspect-auto mix-blend-multiply w-auto" src="images/otter.png"></img>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        The monkey refuses to take help, and continues to copy the crocodile and fails again.
+      </div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+        Disappointed by his inability to swim, he picks up the smaller rocks from the edge of the lake.
+      </div>
+
+      <img className="md:h-[725px] md:w-[1450px] aspect-auto mix-blend-multiply w-auto" src="images/small-rocks.png"></img>
 
     </div>
   );
