9ce5508caf60e5fe552eb62b9e99c539de7e4681
malayvasa <malayvasa@gmail.com>
2022-11-26 10:34:43 +0530

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen min-h-screen py-48 text-center bg-stone-100 text-stone-800 font-sans space-y-48 flex flex-col items-center justify-center">
+      
+      <div className="flex flex-col space-y-12 items-center">
+      <div className="text-7xl w-1/2">The monkey that did everything <br/> on his own</div>
+      <div className="text-2xl">by Malay Vasa</div>
+      </div>
+      
+
+
+      <div className="relative md:h-[512px] md:w-[1024px] aspect-auto w-auto">
+        <img className="md:h-[512px] md:w-[1024px] aspect-auto mix-blend-multiply w-auto" src="images/forest.png"></img>
+        <img className="md:h-[512px] md:w-[1024px] aspect-auto w-auto absolute top-0 animate-moveColdOne" src="images/cold-1.png"></img>
+        <img className="md:h-[512px] md:w-[1024px] aspect-auto w-auto absolute top-0 animate-moveColdTwo animation-delay-1000" src="images/cold-2.png"></img>
+      </div>
+      
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">It was a cold winter morning, the forest was covered in a thick fog.</div>
+
+      <img className="md:h-[725px] md:w-[725px] aspect-auto mix-blend-multiply w-auto" src="images/owl.png"></img>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">The wise owl, sat on a branch and looked down on the forest floor.</div>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">He saw the animals curled up together, trying to save all the warmth they had.</div>
+
+      <img className="md:h-[612px] md:w-[1224px] aspect-auto mix-blend-multiply w-auto" src="images/animals.png"></img>
+
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">In this bunch, there was a monkey in the corner.</div>
+      
+      <div className="flex flex-col text-3xl md:text-5xl">
+        <div className="flex-shrink-0">
+          Even though 
+        </div>
+        
+        <div className="">
+          he was <span className="text-8xl inline-block shake-little shake-constant"> shivering,</span>
+        </div>
+
+        <img className="md:h-[512px] md:w-[512px] aspect-auto mix-blend-multiply w-auto shake-little shake-constant" src="images/monkey.png"></img>
+      </div>
+
+      
+      
+      <div className="text-3xl mx-12 md:text-5xl w-1/2">
+          he did not sit with the others.
+      </div>
+
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    animationDelay: {
+      100: "100ms",
+      200: "200ms",
+      300: "300ms",
+      400: "400ms",
+      1000:"1000ms",
+    },
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        moveCold1: {
+          '0%, 100%': { transform: 'translateY(0)', opacity: '20%'},
+          '50%': { transform: 'translateY(-10px)', opacity: '100%'}
+        },
+        moveCold2: {
+          '0%, 100%': { transform: 'translateY(0)', opacity: '100%'},
+          '50%': { transform: 'translateY(10px)', opacity: '20%'}
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        moveColdOne: 'moveCold1 15s ease-in-out infinite',
+        moveColdTwo: 'moveCold2 15s ease-in-out infinite',
+      },
+    },
+    fontFamily: {
+      sans: ['LORE', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
